1ba0af02d0c8ef8542b042cc581c7d8a90e0789b
suntorytime <stevensm90@gmail.com>
2016-01-22 16:59:03 -0800

    Add Profile page with past bids

--- auction/client/templates/profile.js
+++ auction/client/templates/profile.js
@@ -1,8 +1,19 @@
 if (Meteor.isClient) {
   Template.profile.helpers({
-    findUserBids: function() {
-      return Bids.find({'user': Meteor.userId()}).fetch()
-      // return Bids.find()
+    findUserArtworkObjects: function() {
+      var myBids = Bids.find({'user': Meteor.userId()}).fetch();
+      var myArtworkIds = [];
+      var myArtworkObjects = [];
+      for (var i = 0; i < myBids.length; i++) {
+        myArtworkIds.push(myBids[i].artId)
+      };
+
+      for (var i = 0; i < myArtworkIds.length; i++) {
+        var myArtworkObject = Images.findOne(myArtworkIds[i]);
+        myArtworkObjects.push(myArtworkObject.url())
+      };
+      var uniqueURL = myArtworkObjects.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
+      return uniqueURL
     }
   })
 }
